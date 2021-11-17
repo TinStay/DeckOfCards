3091e512e520353ad63614ddc83966e16f56df50
Martin <marti.staykov@gmail.com>
2021-11-17 17:17:57 +0100

    Add Rank and Suit types

--- card.go
+++ card.go
@@ -1,5 +1,33 @@
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+type Rank uint8 
+
+const (
+	_ Rank = iota
+	Ace,
+	Two,
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
 
 type Card struct {
-	Number string
-	Rank string
+	Suit
+	Rank 
 }
+
